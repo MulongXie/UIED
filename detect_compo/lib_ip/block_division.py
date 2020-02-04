beb67f0dd039c4b9971a98335c8974c118d2995f
Mulong <dsh15325@163.com>
2020-02-04 16:35:44 +1100

    remove the top bar block

--- detect_compo/lib_ip/block_division.py
+++ detect_compo/lib_ip/block_division.py
@@ -10,75 +10,9 @@ import lib_ip.ip_draw as draw
 import lib_ip.ip_segment as seg
 
 
-def block_division(grey, show=False, write_path=None):
-    '''
-    :param grey: grey-scale of original image
-    :return: corners: list of [(top_left, bottom_right)]
-                        -> top_left: (column_min, row_min)
-                        -> bottom_right: (column_max, row_max)
-    '''
-
-    def flood_fill_bfs(img, x_start, y_start, mark):
-        '''
-        Identify the connected region based on the background color
-        :param img: grey-scale image
-        :param x_start: row coordinate of start position
-        :param y_start: column coordinate of start position
-        :param mark: record passed points
-        :return: region: list of connected points
-        '''
-        def neighbor(x, y):
-            for i in range(x - 1, x + 2):
-                if i < 0 or i >= img.shape[0]: continue
-                for j in range(y - 1, y + 2):
-                    if j < 0 or j >= img.shape[1]: continue
-                    if mark[i, j] == 0 and abs(img[i, j] - img[x, y]) < 8:
-                        stack.append([i, j])
-                        mark[i, j] = 255
-        stack = [[x_start, y_start]]  # points waiting for inspection
-        region = [[x_start, y_start]]  # points of this connected region
-        mark[x_start, y_start] = 255  # drawing broad
-        while len(stack) > 0:
-            point = stack.pop()
-            region.append(point)
-            neighbor(point[0], point[1])
-        return region
-
-    blocks = []
-    mask = np.zeros((grey.shape[0], grey.shape[1]), dtype=np.uint8)
-    broad = np.zeros((grey.shape[0], grey.shape[1], 3), dtype=np.uint8)
-
-    row, column = grey.shape[0], grey.shape[1]
-    for x in range(row):
-        for y in range(column):
-            if mask[x, y] == 0:
-                region = flood_fill_bfs(grey, x, y, mask)
-                # ignore small regions
-                if len(region) < 500:
-                    continue
-                # get the boundary of this region
-                boundary = util.boundary_get_boundary(region)
-                # ignore lines
-                if util.boundary_is_line(boundary, 5):
-                    continue
-                # ignore non-rectangle as blocks must be rectangular
-                if not util.boundary_is_rectangle(boundary, 0.66, 0.25):
-                    continue
-                blocks.append(boundary)
-                draw.draw_region(region, broad)
-    if show:
-        cv2.imshow('broad', broad)
-        cv2.waitKey()
-    if write_path is not None:
-        cv2.imwrite(write_path, broad)
-
-    blocks_corner = det.get_corner(blocks)
-    return blocks_corner
-
-
 def block_rectify(block_corner, components_corner):
     '''
-    Assemble the split
+    correct the coordinates of compos to the holistic image
     :param block_corner: corners of blocks
                         (top_left, bottom_right)
                         -> top_left: (column_min, row_min)
@@ -127,3 +61,83 @@ def block_erase(binary, blocks_corner, show=False, pad=2):
         cv2.imshow('after', binary)
         cv2.waitKey()
     return binary
+
+
+def block_rm_top_bar(blocks_corner, org_shape):
+    blocks_corner_new = []
+    for block in blocks_corner:
+        ((column_min, row_min), (column_max, row_max)) = block
+        if column_min < 5 and row_min < 5 and\
+                org_shape[1] - row_max < 5 and column_max < 100:
+            blocks_corner_new.append(block)
+
+    return blocks_corner_new
+
+
+def block_division(grey, show=False, write_path=None):
+    '''
+    :param grey: grey-scale of original image
+    :return: corners: list of [(top_left, bottom_right)]
+                        -> top_left: (column_min, row_min)
+                        -> bottom_right: (column_max, row_max)
+    '''
+
+    def flood_fill_bfs(img, x_start, y_start, mark):
+        '''
+        Identify the connected region based on the background color
+        :param img: grey-scale image
+        :param x_start: row coordinate of start position
+        :param y_start: column coordinate of start position
+        :param mark: record passed points
+        :return: region: list of connected points
+        '''
+
+        def neighbor(x, y):
+            for i in range(x - 1, x + 2):
+                if i < 0 or i >= img.shape[0]: continue
+                for j in range(y - 1, y + 2):
+                    if j < 0 or j >= img.shape[1]: continue
+                    if mark[i, j] == 0 and abs(img[i, j] - img[x, y]) < 8:
+                        stack.append([i, j])
+                        mark[i, j] = 255
+
+        stack = [[x_start, y_start]]  # points waiting for inspection
+        region = [[x_start, y_start]]  # points of this connected region
+        mark[x_start, y_start] = 255  # drawing broad
+        while len(stack) > 0:
+            point = stack.pop()
+            region.append(point)
+            neighbor(point[0], point[1])
+        return region
+
+    blocks = []
+    mask = np.zeros((grey.shape[0], grey.shape[1]), dtype=np.uint8)
+    broad = np.zeros((grey.shape[0], grey.shape[1], 3), dtype=np.uint8)
+
+    row, column = grey.shape[0], grey.shape[1]
+    for x in range(row):
+        for y in range(column):
+            if mask[x, y] == 0:
+                region = flood_fill_bfs(grey, x, y, mask)
+                # ignore small regions
+                if len(region) < 500:
+                    continue
+                # get the boundary of this region
+                boundary = util.boundary_get_boundary(region)
+                # ignore lines
+                if util.boundary_is_line(boundary, 5):
+                    continue
+                # ignore non-rectangle as blocks must be rectangular
+                if not util.boundary_is_rectangle(boundary, 0.66, 0.25):
+                    continue
+                blocks.append(boundary)
+                draw.draw_region(region, broad)
+    if show:
+        cv2.imshow('broad', broad)
+        cv2.waitKey()
+    if write_path is not None:
+        cv2.imwrite(write_path, broad)
+
+    blocks_corner = det.get_corner(blocks)
+    blocks_corner = block_rm_top_bar(blocks_corner, grey.shape)
+    return blocks_corner
