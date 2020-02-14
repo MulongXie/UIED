b8c703be5f87ccad46736bc9e0081d260084ed27
Mulong <dsh15325@163.com>
2020-02-14 18:22:29 +1100

    adjust width threshold for block compo

--- config/CONFIG_UIED.py
+++ config/CONFIG_UIED.py
@@ -12,7 +12,7 @@ class Config:
         self.THRESHOLD_REC_MAX_DENT_RATIO = 0.25
         self.THRESHOLD_LINE_THICKNESS = 8
         self.THRESHOLD_LINE_MIN_LENGTH = 0.9
-        self.THRESHOLD_COMPO_MAX_SCALE = (0.1, 0.9)  # (80/800, 405/450) maximum height and width ratio for a atomic compo (button)
+        self.THRESHOLD_COMPO_MAX_SCALE = (0.1, 0.95)  # (80/800, 422.5/450) maximum height and width ratio for a atomic compo (button)
         self.THRESHOLD_TEXT_MAX_WORD_GAP = 10
         self.THRESHOLD_TEXT_MAX_HEIGHT = 0.05  # 40/800 maximum height of text
         self.THRESHOLD_TOP_BOTTOM_BAR = (0.045, 0.94)  # (36/800, 752/800) height ratio of top and bottom bar
