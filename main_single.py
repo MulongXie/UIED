4b6c45ab97a308b075d2b5558afb94545c6e7595
Mulong <dsh15325@163.com>
2020-03-25 18:09:26 +1100

    update single to be usable

--- main_single.py
+++ main_single.py
@@ -1,32 +1,40 @@
+from os.path import join as pjoin
 import ip_region_proposal as ip
 
 resize_by_height = 800
 
 # set input image path
-PATH_IMG_INPUT = 'E:\\Mulong\\Datasets\\rico\\combined\\23.jpg'
-PATH_OUTPUT_ROOT = 'data\\output'
+# PATH_IMG_INPUT = 'E:\\Mulong\\Datasets\\rico\\combined\\23.jpg'
+input_path_img = 'data\\input\\a.png'
+output_root = 'data\\output'
 
 is_ip = True
-is_clf = False
+is_clf = True
 is_ocr = False
-is_merge = False
+is_merge = True
 
 if is_ocr:
     import ocr_east as ocr
-    ocr.east(PATH_IMG_INPUT, PATH_OUTPUT_ROOT,
-             resize_by_height=resize_by_height, show=True, write_img=True)
+    ocr.east(input_path_img, output_root, resize_by_height=None, show=False, write_img=True)
 
 if is_ip:
-    # Turn on classification
+    # switch of the classification func
+    classifier = None
     if is_clf:
-        from Resnet import ResClassifier
-        classifier = ResClassifier()
-    else:
-        classifier = None
-    ip.compo_detection(PATH_IMG_INPUT, PATH_OUTPUT_ROOT, classifier=classifier,
-                       resize_by_height=resize_by_height, show=True, write_img=True)
+        classifier = {}
+        from CNN import CNN
+
+        classifier['Image'] = CNN('Image')
+        classifier['Elements'] = CNN('Elements')
+        classifier['Noise'] = CNN('Noise')
+
+    ip.compo_detection(input_path_img, output_root, resize_by_height=resize_by_height, show=False,
+                       classifier=classifier)
 
 if is_merge:
     import merge
-    merge.incorporate(PATH_IMG_INPUT, PATH_OUTPUT_ROOT,
-                      resize_by_height=resize_by_height, show=True, write_img=True)
+    name = input_path_img.split('\\')[-1][:-4]
+    compo_path = pjoin(output_root, 'ip', str(name) + '.json')
+    ocr_path = pjoin(output_root, 'ocr', str(name) + '.json')
+    merge.incorporate(input_path_img, compo_path, ocr_path, output_root, resize_by_height=resize_by_height, show=True,
+                      write_img=True)
