32f359808453a599ee7131fb9fc363709f2e648b
MulongXie <dsh15325@163.com>
2020-08-07 23:19:48 +1000

    update

--- run_single.py
+++ run_single.py
@@ -2,8 +2,6 @@ from os.path import join as pjoin
 import cv2
 import os
 
-import detect_compo.ip_region_proposal as ip
-
 
 def resize_height_by_longest_edge(img_path, resize_length=800):
     org = cv2.imread(img_path)
@@ -28,14 +26,15 @@ if __name__ == '__main__':
     is_merge = True
 
     if is_ocr:
-        os.makedirs(pjoin(output_root, 'ocr'), exist_ok=True)
         import detect_text_east.ocr_east as ocr
         import detect_text_east.lib_east.eval as eval
+        os.makedirs(pjoin(output_root, 'ocr'), exist_ok=True)
         models = eval.load()
         ocr.east(input_path_img, output_root, models,
                  resize_by_height=resized_height, show=False)
 
     if is_ip:
+        import detect_compo.ip_region_proposal as ip
         os.makedirs(pjoin(output_root, 'ip'), exist_ok=True)
         # switch of the classification func
         classifier = None
