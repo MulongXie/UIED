b90a7cdea900aafd06ccf656de3761bd196a273e
MulongXie <dsh15325@163.com>
2020-10-06 08:27:54 +1100

    add parameter record as comment

--- run_single.py
+++ run_single.py
@@ -21,6 +21,10 @@ if __name__ == '__main__':
         text:max-word-gap: words with smaller distance than the gap are counted as a line
         text:max-line-gap: lines with smaller distance than the gap are counted as a paragraph
     '''
+    '''
+    mobile: {'min-grad':5, 'ffd-block':5, 'min-ele-area':50, 'max-word-gap':4, 'max-line-gap':4}
+    web:    {'min-grad':4, 'ffd-block':5, 'min-ele-area':25, 'max-word-gap':4, 'max-line-gap':4}
+    '''
     key_params = {'min-grad':5, 'ffd-block':5, 'min-ele-area':25,
                   'max-word-gap':4, 'max-line-gap':4}
 
