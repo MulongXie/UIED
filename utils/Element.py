9edc1d7077854779722c38a11052b7bb46920280
MulongXie <dsh15325@163.com>
2021-07-02 14:54:08 +1000

    update element object

--- utils/Element.py
+++ utils/Element.py
@@ -1,49 +1,84 @@
 import numpy as np
-from detect_compo.lib_ip.Bbox import Bbox
+import cv2
 
 
 class Element:
     def __init__(self, corner, category):
         self.category = category
-        self.bbox = Bbox(corner[0], corner[1], corner[2], corner[3])
-        self.area = self.bbox.box_area
-        self.width = self.bbox.width
-        self.height = self.bbox.height
+        self.col_min, self.row_min, self.col_max, self.row_max = corner
+        self.width = self.col_max - self.col_min
+        self.height = self.row_max - self.row_min
+        self.area = self.width * self.height
+
+        self.is_child = False
+        self.children = []  # list of elements
+
+    def init_bound(self):
+        self.width = self.col_max - self.col_min
+        self.height = self.row_max - self.row_min
+        self.area = self.width * self.height
 
     def put_bbox(self):
-        return self.bbox.put_bbox()
+        return self.col_min, self.row_min, self.col_max, self.row_max
 
-    def element_relation(self, element_b, bias=(0, 0)):
+    def resize(self, resize_ratio):
+        self.col_min = int(self.col_min * resize_ratio)
+        self.row_min = int(self.row_min * resize_ratio)
+        self.col_max = int(self.col_max * resize_ratio)
+        self.row_max = int(self.row_max * resize_ratio)
+        self.init_bound()
+
+    def element_merge(self, element_b, new_element=False, new_category=None):
+        col_min_a, row_min_a, col_max_a, row_max_a = self.put_bbox()
+        col_min_b, row_min_b, col_max_b, row_max_b = element_b.put_bbox()
+        new_corner = (min(col_min_a, col_min_b), min(row_min_a, row_min_b), max(col_max_a, col_max_b), max(row_max_a, row_max_b))
+        if new_element:
+            return Element(new_corner, new_category)
+        else:
+            self.col_min, self.row_min, self.col_max, self.row_max = new_corner
+            self.init_bound()
+
+    def calc_intersection_area(self, element_b, bias=0):
+        a = self.put_bbox()
+        b = element_b.put_bbox()
+        col_min_s = max(a[0], b[0]) - bias
+        row_min_s = max(a[1], b[1]) - bias
+        col_max_s = min(a[2], b[2])
+        row_max_s = min(a[3], b[3])
+        w = np.maximum(0, col_max_s - col_min_s)
+        h = np.maximum(0, row_max_s - row_min_s)
+        inter = w * h
+
+        iou = inter / (self.area + element_b.area - inter)
+        ioa = inter / self.area
+        iob = inter / element_b.area
+
+        return inter, iou, ioa, iob
+
+    def element_relation(self, element_b, bias=0):
         """
         :return: -1 : a in b
                  0  : a, b are not intersected
                  1  : b in a
                  2  : a, b are identical or intersected
         """
-        return self.bbox.bbox_relation_nms(element_b.bbox, bias)
+        inter, iou, ioa, iob = self.calc_intersection_area(element_b, bias)
 
-    def element_merge(self, element_b, new_element=False, new_category=None):
-        if not new_element:
-            self.bbox = self.bbox.bbox_merge(element_b.bbox)
-        else:
-            bbox = self.bbox.bbox_merge(element_b.bbox)
-            element = Element(bbox.put_bbox(), new_category)
-            return element
+        # area of intersection is 0
+        if ioa == 0:
+            return 0
+        # a in b
+        if ioa > 0.6:
+            return -1
+        # b in a
+        if iob > 0.6:
+            return 1
+        return 2
 
-    def calc_intersection_area(self, element_b):
-        a = self.put_bbox()
-        b = element_b.put_bbox()
-        col_min_s = max(a[0], b[0])
-        row_min_s = max(a[1], b[1])
-        col_max_s = min(a[2], b[2])
-        row_max_s = min(a[3], b[3])
-        w = np.maximum(0, col_max_s - col_min_s)
-        h = np.maximum(0, row_max_s - row_min_s)
-        inter = w * h
-        return inter
-
-    def calc_iou(self, element_b):
-        inter = self.calc_intersection_area(element_b)
-        iou = inter / (self.area + element_b.area - inter)
-        return iou
-
+    def visualize_element(self, img, color=(0, 255, 0), line=1, show=False):
+        loc = self.put_bbox()
+        cv2.rectangle(img, loc[:2], loc[2:], color, line)
+        if show:
+            cv2.imshow('element', img)
+            cv2.waitKey(0)
+            cv2.destroyWindow('element')
