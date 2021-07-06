bda3c0d5647303cf8f6413a0984164d78740971e
MulongXie <dsh15325@163.com>
2021-07-06 11:47:52 +1000

    refactoring

--- detect_compo/ip_region_proposal.py
+++ detect_compo/ip_region_proposal.py
@@ -7,9 +7,8 @@ import numpy as np
 import detect_compo.lib_ip.ip_preprocessing as pre
 import detect_compo.lib_ip.ip_draw as draw
 import detect_compo.lib_ip.ip_detection as det
-import detect_compo.lib_ip.ip_segment as seg
 import detect_compo.lib_ip.file_utils as file
-import detect_compo.lib_ip.block_division as blk
+import detect_compo.lib_ip.Block as blk
 import detect_compo.lib_ip.Component as Compo
 from config.CONFIG_UIED import Config
 C = Config()
@@ -65,8 +64,7 @@ def nesting_inspection(org, grey, compos, ffl_block):
 
 
 def compo_detection(input_img_path, output_root, uied_params,
-                    resize_by_height=600,
-                    classifier=None, show=False, wai_key=0):
+                    resize_by_height=800, classifier=None, show=False, wai_key=0):
 
     start = time.clock()
     name = input_img_path.split('/')[-1][:-4]
@@ -78,13 +76,11 @@ def compo_detection(input_img_path, output_root, uied_params,
 
     # *** Step 2 *** element detection
     det.rm_line(binary, show=show, wait_key=wai_key)
-    # det.rm_line_v_h(binary, show=show)
     uicompos = det.component_detection(binary, min_obj_area=int(uied_params['min-ele-area']))
     # draw.draw_bounding_box(org, uicompos, show=show, name='components', wait_key=wai_key)
 
     # *** Step 3 *** results refinement
-    uicompos = det.merge_intersected_corner(uicompos, org, is_merge_contained_ele=uied_params['merge-contained-ele'],
-                                            max_gap=(0, 0), max_ele_height=25)
+    uicompos = det.merge_intersected_corner(uicompos, org, is_merge_contained_ele=uied_params['merge-contained-ele'], max_gap=(0, 0), max_ele_height=25)
     Compo.compos_update(uicompos, org.shape)
     Compo.compos_containment(uicompos)
     # draw.draw_bounding_box(org, uicompos, show=show, name='merged', wait_key=wai_key)
@@ -110,7 +106,7 @@ def compo_detection(input_img_path, output_root, uied_params,
 
     # *** Step 6 *** element classification: all category classification
     if classifier is not None:
-        classifier['Elements'].predict(seg.clipping(org, uicompos), uicompos)
+        classifier['Elements'].predict([compo.compo_clipping(org) for compo in uicompos], uicompos)
         draw.draw_bounding_box_class(org, uicompos, show=show, name='cls', write_path=pjoin(ip_root, 'result.jpg'))
         draw.draw_bounding_box_class(org, uicompos, write_path=pjoin(output_root, 'result.jpg'))
 
@@ -119,7 +115,5 @@ def compo_detection(input_img_path, output_root, uied_params,
     file.save_corners_json(pjoin(output_root, 'compo.json'), uicompos)
     # seg.dissemble_clip_img_fill(pjoin(output_root, 'clips'), org, uicompos)
 
-    cv2.destroyAllWindows()
     print("[Compo Detection Completed in %.3f s] %s" % (time.clock() - start, input_img_path))
-    # if show:
-    #     cv2.destroyAllWindows()
+    if show: cv2.destroyAllWindows()
